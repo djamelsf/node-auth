076eeaad1e14274e8ce7e307f15e322c153cff76
DJAM <sefsafd@gmail.com>
2021-10-16 18:57:35 +0200

    v2

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 const app = express()
 
 // middleware
-app.use(express.static('public'))
 app.use(express.json())
 app.use(cookieParser());
 
